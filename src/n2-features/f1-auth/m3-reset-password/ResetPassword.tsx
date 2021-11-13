76fccdd4c0ba183e94102a283d4c06988a999a5c
Arseny <arsbazel@gmail.com>
2021-11-13 20:50:15 +0300

    ui-components

--- src/n2-features/f1-auth/m3-reset-password/ResetPassword.tsx
+++ src/n2-features/f1-auth/m3-reset-password/ResetPassword.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import SuperInputText from "../../../n1-main/n1-ui/common/c2-input/SuperInputText";
+import s from "./ResetPassword.module.css"
+import SuperButton from "../../../n1-main/n1-ui/common/c1-button/SuperButton";
+import {NavLink} from "react-router-dom";
 
 
 export function ResetPassword() {
-    return(
-        <div>
-            <h1>Reset Password</h1>
-        </div>
+    return (
+        <form className={s.form}>
+            <div className={s.container}>
+                <span className={s.Title}>it-cards</span>
+                <h1 className={s.subTitle}>Forgot your password?</h1>
+                <SuperInputText type="text" required name="Email"/>
+                <span >Enter your email address and we will send you further instructions </span>
+                <SuperButton  children={"Send Instructions"}/>
+                <span>Did you remember your password?</span>
+                <NavLink children="Try logging in" to="/login"/>
+            </div>
+        </form>
     )
 }
--- src/n2-features/f1-auth/m4-new-password/NewPassword.tsx
+++ src/n2-features/f1-auth/m4-new-password/NewPassword.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {MouseEvent, useState} from "react";
+import s from "./NewPassword.module.css";
+import SuperInputText from "../../../n1-main/n1-ui/common/c2-input/SuperInputText";
+import SuperButton from "../../../n1-main/n1-ui/common/c1-button/SuperButton";
 
 
 export function NewPassword() {
-    return(
-        <div>
-            <h1>New Registration</h1>
-        </div>
+
+
+    return (
+        <form className={s.form}>
+            <div className={s.container}>
+                <span className={s.Title}>it-cards</span>
+                <h1 className={s.subTitle}>Create new password</h1>
+                <SuperInputText type="text" required name="Email"/>
+                <span className={s.description}>Create new password and we will send you further instructions to email</span>
+                <SuperButton />
+            </div>
+        </form>
     )
 }
