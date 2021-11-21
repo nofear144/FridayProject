08d6af4b0a32d5f68c08ca8deb9704d3c37da201
Andrei1989g <andreyazarov1989g@gmail.com>
2021-11-21 11:28:12 +0200

    add cards-reducer.ts

---
+++ src/n1-main/n2-bll/reducers/cards-reducer.ts
@@ -0,0 +1,23 @@
+
+const initialState = {
+
+}
+
+
+export const cardsReducer = (state:initialStateType=initialState, action: any):initialStateType => {
+    switch (action.type) {
+
+        default:
+            return state
+    }
+}
+
+//Actions
+
+
+//Thunks
+
+
+
+//Types
+type initialStateType = typeof initialState
